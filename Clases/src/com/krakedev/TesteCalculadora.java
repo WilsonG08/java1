3d4855e3b69d64501fec3ef72893d02f7b3b1ef7
WilsonG08 <wilson.guayanay@epn.edu.ec>
2023-12-08 13:22:16 -0500

    Funcion mostrar resultado

--- Clases/src/com/krakedev/TesteCalculadora.java
+++ Clases/src/com/krakedev/TesteCalculadora.java
@@ -20,14 +20,15 @@ public class TesteCalculadora {
 		resultadoSuma = calcu.sumar(5, 3);
 		resultadoResta = calcu.restar(10, 7);
 		resultadoMultiplicacion = calcu.multiplicar(10, 5);
-		resultadoDivision= calcu.division(64, 8);
+		resultadoDivision = calcu.division(64, 8);
 		resultadoPromedio = calcu.promedio(10, 9, 8);
 
 		System.out.println("RESULTADO SUMA: " + resultadoSuma);
 		System.out.println("RESULTADO RESTA: " + resultadoResta);
-		System.out.println("RESULTADO MULTIPLICACION: " +resultadoMultiplicacion);
+		System.out.println("RESULTADO MULTIPLICACION: " + resultadoMultiplicacion);
 		System.out.println("RESULTADO DIVISION: " + resultadoDivision);
 		System.out.println("RESULTADO PROMEDIO: " + resultadoPromedio);
+		calcu.mostrarResultado();
 
 	}
 
